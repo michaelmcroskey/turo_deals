b8bbd6ca51eed87ffa4138c801b6542e90143ea6
Michael McRoskey <michael.mcroskey@gmail.com>
2020-06-21 15:31:16 -0700

    Lint file and clean up exceptions
    - Also adds @retry decorator

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # turo_deals
- Find deals on Turo car rentals.
+
+Find deals on Turo car rentals.
 
 ## Setup
-```
+
+```bash
 python3 -m venv env
 source env/bin/activate
 pip3 install -r requirements.txt
@@ -11,7 +13,8 @@ export GOOGLE_APPLICATION_CREDENTIALS="/path/to/credentials.json"
 ```
 
 ## Usage
-```
+
+```bash
 python3 cheapest_model_3.py \
  --zip_code 94040 \
  --num_future_weekends 3
--- cheapest_model_3.py
+++ cheapest_model_3.py
@@ -1,106 +1,121 @@
+'''Scrape Turo for future bargain rentals.'''
+# pylint: disable=broad-except
+
 import argparse
 import datetime
 import logging
-import pandas as pd
-import pgeocode
 import re
-import requests
 import string
 
-from google.cloud import bigquery
+import pandas as pd
+import pgeocode
+import requests
 from bs4 import BeautifulSoup
+from google.cloud import bigquery
+from retry import retry
 
 TURO_ROOT_URL = 'https://turo.com'
 
-FRIDAY = 5
-SATURDAY = 6
-SUNDAY = 7
+# Access DAYS by index, first element '' since we use isoweekday (shifted 1)
+DAYS = ('', 'MONDAY', 'TUESDAY', 'WEDNESDAY',
+        'THURSDAY', 'FRIDAY', 'SATURDAY', 'SUNDAY')
 
 
 def printable(full_string):
-  printable = set(string.printable)
-  return ''.join([x for x in full_string if x in printable])
+  return ''.join([x for x in full_string if x in set(string.printable)])
 
 
-def dayOfWeek(weekday, date):
-  shift = weekday - date.isoweekday()
-  return(date + datetime.timedelta(days=shift))
-  
+def day_of_week(weekday, date):
+  return date + datetime.timedelta(days=weekday - date.isoweekday())
 
-def datesInScope(weeks_ahead):
+
+def dates_in_scope(weeks_ahead):
   today = datetime.date.today()
-  if dayOfWeek(FRIDAY, today) < today:
+  if day_of_week(DAYS.index('FRIDAY'), today) < today:
     today += datetime.timedelta(weeks=1)
   return [today + datetime.timedelta(weeks=w) for w in range(weeks_ahead)]
 
-    
-def getTuroListings(startDate, endDate, zip_code, latitude, longitude, maxMiles=20):
+
+@retry(delay=1, backoff=2, max_delay=8)
+def get_turo_listings(start, end, zip_code, latitude, longitude, max_miles):
+  '''Request Turo rental listings within start and end dates for a location.'''
   headers = {
-    'Pragma': 'no-cache',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,es-US;q=0.8,es;q=0.6,ru-BY;q=0.4,ru;q=0.2,en;q=0.2',
-    'Accept': '*/*',
-    'Referer': TURO_ROOT_URL + '/search?',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'Accept-Language': 'en-US,es-US;q=0.8,es;',
+      'Accept': '*/*',
+      'Referer': TURO_ROOT_URL + '/search?',
+      'Connection': 'keep-alive',
+      'Cache-Control': 'no-cache',
   }
 
   params = (
       ('country', 'US'),
-      ('endDate', endDate),
+      ('endDate', end),
       ('endTime', '10:00'),
       ('itemsPerPage', '200'),
       ('location', str(zip_code)),
       ('locationType', 'ZIP'),
-      ('maximumDistanceInMiles', str(maxMiles)),
+      ('maximumDistanceInMiles', str(max_miles)),
       ('Latitude', str(latitude)),
       ('Longitude', str(longitude)),
       ('sortType', 'RELEVANCE'),
-      ('startDate', startDate),
+      ('startDate', start),
       ('startTime', '10:00'),
       ('makes', 'Tesla'),
       ('models', 'Model 3'),
   )
   result = None
-  for i in range(3):
-    url_to_fetch = requests.Request('GET', TURO_ROOT_URL + '/api/search', params=params, headers=headers).prepare().url
-    print(f'Requesting URL:\n\t{url_to_fetch}')
-    try:
-      result = requests.get(TURO_ROOT_URL + '/api/search', headers=headers, params=params, timeout=15)
-      if result != None:
-        print('Recieved response.\n')
-        break
-    except Exception as e:
-      logging.warn(e)
-  return result.json()
-  
+  url_to_fetch = requests.Request(
+      'GET',
+      TURO_ROOT_URL + '/api/search',
+      params=params,
+      headers=headers).prepare().url
+  print(f'Requesting URL:\n\t{url_to_fetch}')
+  try:
+    result = requests.get(TURO_ROOT_URL + '/api/search',
+                          headers=headers, params=params, timeout=15)
+    if result is not None:
+      print('Recieved response.\n')
+      return result.json()
+  except Exception as e:
+    logging.warning('Exception while requesting listings: %s', e)
 
-class Car(dict):
+
+class Car():
+  '''Represents an individual car rental.'''
+
   def __init__(self, car_json=None, vehicle_url=None):
     if car_json:
       self.date_accessed = datetime.date.today()
-      self.instantBookDisplayed = bool(car_json['instantBookDisplayed'])
+      self.instant_book = bool(car_json['instantBookDisplayed'])
       self.latitude = car_json['location']['latitude']
       self.longitude = car_json['location']['longitude']
-      self.allStarHost = bool(car_json['owner']['allStarHost'])
-      self.averageDailyPrice = float(car_json['rate']['averageDailyPrice'])
-      self.rating = float(car_json['rating'])
-      self.reviewCount = int(car_json['reviewCount'])
-      self.renterTripsTaken = int(car_json['renterTripsTaken'])
-      self.vehicle_trim = car_json['vehicle']['trim']
+      self.all_star_host = bool(car_json['owner']['allStarHost'])
+      self.average_daily_price = float(car_json['rate']['averageDailyPrice'])
+      rating = car_json['rating']
+      self.rating = float(rating) if rating else None
+      self.review_count = int(car_json['reviewCount'])
+      self.renter_trips_taken = int(car_json['renterTripsTaken'])
+      self.vehicle_trim = str(car_json['vehicle']['trim'])
       self.vehicle_year = int(car_json['vehicle']['year'])
-      self.vehicle_url = TURO_ROOT_URL + car_json['vehicle']['url']
+      self.vehicle_url = str(TURO_ROOT_URL + car_json['vehicle']['url'])
     else:
       self.vehicle_url = vehicle_url
-      
+
     self.performance_score = 0
-    self.getDetailedListing()
-  
-  def getDetailedListing(self):
+    try:
+      self.get_detailed_listing()
+    except Exception as e:
+      logging.error('Error requesting detailed listing info: %s', e)
+
+  @retry(delay=1, backoff=2, max_delay=8)
+  def get_detailed_listing(self):
+    '''Request details not provided in initial result. (e.g. description)'''
     assert self.vehicle_url is not None
     page = requests.get(self.vehicle_url, timeout=15)
     soup = BeautifulSoup(page.content, 'html.parser')
-    
+
     # Trim
     trims = []
     for label in soup.find_all('div', {'class': 'vehicleLabel'}):
@@ -112,75 +127,86 @@ class Car(dict):
     self.performance_trim = self.trim == 'performance'
     if self.performance_trim:
       self.performance_score += 1
-     
+
     # Description
-    self.description = set([printable(x.text) for x in soup.find_all('div', {'class': 'vehicleDetails-descriptionText'})]).pop()
-    self.performance_description = any([re.findall(r'performance', x.lower()) for x in self.description])
+    self.description = set([printable(x.text) for x in soup.find_all(
+        'div', {'class': 'vehicleDetails-descriptionText'})]).pop()
+    self.performance_description = any(
+        [re.findall(r'performance', x.lower()) for x in self.description])
     if self.performance_description:
       self.performance_score += 1
-    
+
     # Allowed mileage
-    reservation_box = set([x.text for x in soup.find_all('div', {'class': 'reservationBox'})]).pop()
-    allowed_mileage = re.findall(r'Distance includedDay(\d+ mi|Unlimited)Week(\d+ mi|Unlimited)Month(\d+ mi|Unlimited)', reservation_box)
+    reservation_box = set([x.text for x in soup.find_all(
+        'div', {'class': 'reservationBox'})]).pop()
+    allowed_mileage = re.findall(
+        r'Distance includedDay(\d+ mi|Unlimited)Week(\d+ mi|Unlimited)'
+        r'Month(\d+ mi|Unlimited)', reservation_box)
     if allowed_mileage:
       self.day_miles, self.week_miles, self.month_miles = allowed_mileage[0]
 
 
-def createDataset(client, dataset_name):
+@retry(delay=1, backoff=2, max_delay=8)
+def create_dataset(client, dataset_name):
   dataset_id = "{}.{}".format(client.project, dataset_name)
   dataset = bigquery.Dataset(dataset_id)
   dataset.location = "US"
   dataset = client.create_dataset(dataset)
-  logging.info("Created dataset {}".format(dataset_id))
+  logging.info("Created dataset %s", dataset_id)
   return dataset_id
 
 
-def createOrAsserDataset(client, dataset_name):
+@retry(delay=1, backoff=2, max_delay=8)
+def create_or_assert_dataset(client, dataset_name):
+  '''Create Biquery dataset if it does not already exist.'''
   datasets = list(client.list_datasets())
   project = client.project
   if datasets:
     if dataset_name not in [x.dataset_id for x in datasets]:
-      createDataset(client, dataset_name)
+      create_dataset(client, dataset_name)
   else:
-    logging.info("{} project does not contain any datasets.".format(project))
-    createDataset(client, dataset_name)
+    logging.info("%s project does not contain any datasets.", project)
+    create_dataset(client, dataset_name)
   return dataset_name
 
 
-def uploadTable(client, table_id, df):  
+@retry(delay=1, backoff=2, max_delay=8)
+def upload_table(client, table_id, df):
   # Create/replace current table
   job_config = bigquery.LoadJobConfig(write_disposition='WRITE_TRUNCATE')
   job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
   job.result()
-  logging.info(f'Successfully uploaded DataFrame to BigQuery: {table_id}')
+  logging.info('Successfully uploaded DataFrame to BigQuery: %s', table_id)
   return table_id
-    
 
-def main(args):  
+
+def main(request):
   # Zipcode validation and lat/long.
   try:
-    location = pgeocode.Nominatim('us').query_postal_code(args.zip_code)
+    location = pgeocode.Nominatim('us').query_postal_code(request.zip_code)
   except Exception as e:
-    logging.error(f'Not a valid zip code {e}')
+    logging.error('Not a valid zip code %s', e)
     return
-    
+
   uploaded_tables = []
   # Run for number of future weekends specified
-  for date in datesInScope(args.num_future_weekends):
-    start = dayOfWeek(FRIDAY, date)
-    end = dayOfWeek(SUNDAY, date)
-    
+  for date in dates_in_scope(request.num_future_weekends):
+    start = day_of_week(DAYS.index('FRIDAY'), date)
+    end = day_of_week(DAYS.index('SUNDAY'), date)
+
     # Pull turo listings according to flags
     print(f'Getting listings for weekend of {start}.')
-    listings = getTuroListings(startDate=start.strftime('%m/%d/%Y'),
-                               endDate=end.strftime('%m/%d/%Y'),
-                               zip_code=location.postal_code,
-                               latitude=location.latitude,
-                               longitude=location.longitude)['list']
+    listings = get_turo_listings(start=start.strftime('%m/%d/%Y'),
+                                 end=end.strftime('%m/%d/%Y'),
+                                 zip_code=location.postal_code,
+                                 latitude=location.latitude,
+                                 longitude=location.longitude,
+                                 max_miles=request.max_miles)['list']
     if not listings:
-      logging.error('Could not retrieve Turo listings, or Turo responded with an empty list.')
+      logging.error('Could not retrieve Turo listings, '
+                    'or Turo responded with an empty list.')
       return
-    
+
     # Initialize DataFrame
     column_names = vars(Car(listings[0])).keys()
     df = pd.DataFrame(columns=column_names)
@@ -193,36 +219,41 @@ def main(args):
       try:
         rental = Car(listing)
       except Exception as e:
-        print(e, rental)
+        logging.warning('Error storing listing: %s', e)
       row = dict(vars(rental))
       row['weekend'] = start
       df = df.append(row, ignore_index=True)
 
     print(df.drop(['description', 'vehicle_url'], axis=1).head().to_string())
-    
+
     # Upload to BigQuery
     client = bigquery.Client()
     project = client.project
-    dataset_name = createOrAsserDataset(client, str(args.zip_code))
+    dataset_name = create_or_assert_dataset(client, str(location.postal_code))
     table_name = start.strftime('%m_%d_%Y')
     table_id = f'{project}.{dataset_name}.{table_name}'
-    
-    uploaded_tables.append(uploadTable(client, table_id, df))
-  
+
+    uploaded_tables.append(upload_table(client, table_id, df))
+
   print('Successfully uploaded:\n - {}'.format('\n - '.join(uploaded_tables)))
-  
+
 
 if __name__ == '__main__':
   # Parse args
   parser = argparse.ArgumentParser(description='TuroBot')
-  parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-  parser.add_argument('--num_future_weekends', required=True, type=int, help='Weeks ahead')
-  parser.add_argument('--zip_code', required=True, type=int, help='Zip code')
+  parser.add_argument("-v", "--verbose",
+                      help="increase output verbosity", action="store_true")
+  parser.add_argument('--num_future_weekends',
+                      required=True, type=int, help='Weeks ahead')
+  parser.add_argument('--zip_code', required=True, type=str, help='Zip code')
+  parser.add_argument('--max_miles', required=False, type=int,
+                      default=20, help='Maximum search area, in miles.')
   args = parser.parse_args()
-  
+
   # Initialize logging
   if args.verbose:
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.basicConfig(
+        format='%(levelname)s: %(message)s', level=logging.DEBUG)
     logging.info("Verbose output.")
   else:
     logging.basicConfig(format='%(levelname)s: %(message)s')
